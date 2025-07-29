dfff0377ae5ddc4e445f8d9f203524385c4f0194
Russ Treadwell <git@trdwll.com>
2025-07-29 16:03:44 -0400

    add migrations

--- essential_extensions/apps.py
+++ essential_extensions/apps.py
@@ -5,7 +5,3 @@ class EssentialExtensionsConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'essential_extensions'
     verbose_name = 'Essential Extensions'
-
-    def ready(self):
-        """Import signals when the app is ready."""
-        import essential_extensions.signals 
